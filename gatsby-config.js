c7aceb4001ec7f8dc8c6cb0e4c94b89702308057
qiancai <qqzczy@126.com>
2025-01-22 10:24:38 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,9 +103,10 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
-        // https://github.com/gatsbyjs/gatsby/issues/21866#issuecomment-1063668178
-        // Add katex support
-        remarkPlugins: [require("remark-math")],
+        remarkPlugins: [
+          require("remark-math"),
+          [require('remark-footnotes'), { inlineNotes: true }]
+        ],
         rehypePlugins: [[require("rehype-katex"), { strict: "ignore" }]],
         gatsbyRemarkPlugins: [
           {
